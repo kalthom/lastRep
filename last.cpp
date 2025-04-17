9eeb9082d98a55bb3aa8be7fbd4d7165f6c9ca31
kalthom <kalthum.mufath@gmail.com>
2025-04-18 01:13:27 +0300

    just a message

---
+++ last.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+#define ll long long
+#define PI 3.141592653
+
+    
+    
+    int main()
+    {  
+        string s;
+        cin>>s;
+        cout << "Hello , " << s << endl;
+      
+       
+      
+        return 0;
+    }
+    
